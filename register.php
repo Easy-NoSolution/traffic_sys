172ecc5ca69d976820d01c835d3ef2dc3baa8324
易无解 <2198105373@qq.com>
2017-09-22 18:19:36 +0800

    注册测试

--- register.php
+++ register.php
@@ -9,7 +9,7 @@
 require ('connect.php');
 $userId = @$_POST['userId'] ? $_POST['userId'] : NULL;
 $username = @$_POST['username'] ? $_POST['username'] : NULL;
-$userSex = @$_POST['userSex'] ? $_POST['userSex'] : NULL;
+$userSex = @$_POST['userSex'] ? $_POST['userSex'] : 0;
 $userBirthday = @$_POST['userBirthday'] ? $_POST['userBirthday'] : NULL;
 $userAvatar = @$_POST['userAvatar'] ? $_POST['userAvatar'] : NULL;
 $password = @$_POST['password'] ? $_POST['password'] : NULL;
@@ -25,7 +25,7 @@ if (empty($userId) and empty($username) and empty($userSex) and empty($password)
     exit(json_encode($json));
 }
 
-$sql = "insert into user_tb (userId, username, userSex, userBirthday, userAvatar, password) values (''".$userId."', '".$username."', '".$userSex."', '".$userBirthday."', '".$userAvatar."', '".$password."')";
+$sql = "insert into user_tb (userId, username, userSex, userBirthday, userAvatar, password) values (''".$userId."', '".$username."', ".$userSex.", '".$userBirthday."', '".$userAvatar."', '".$password."')";
 $json = array('result' => $sql);
 exit(json_encode($json));
 if (!mysqli_query($connect, $sql)) {
