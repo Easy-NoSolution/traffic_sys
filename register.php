f8fb587cefe3cc59cf91b8176a1fb799b339aeaf
易无解 <2198105373@qq.com>
2017-09-23 00:09:09 +0800

    注册测试

--- register.php
+++ register.php
@@ -10,13 +10,19 @@ require ('connect.php');
 $userId = @$_POST['userId'] ? $_POST['userId'] : NULL;
 $username = @$_POST['username'] ? $_POST['username'] : NULL;
 $userSex = @$_POST['userSex'] ? $_POST['userSex'] : 0;
-$userBirthday = @$_POST['userBirthday'] ? $_POST['userBirthday'] : 'NULL';
+//$userBirthday = @$_POST['userBirthday'] ? $_POST['userBirthday'] : 'NULL';
+//$userBirthday = date('Y-m-d h:i:s');
+$userBirthday = 'NULL';
 $userAvatar = @$_POST['userAvatar'] ? $_POST['userAvatar'] : 'NULL';
 $password = @$_POST['password'] ? $_POST['password'] : NULL;
 
 if (empty($userId) and empty($username) and empty($userSex) and empty($password)) {
     $json = array('result' => 'Some value is NULL');
     exit(json_encode($json));
+}
+
+if ($userAvatar == 'NULL') {
+    $userAvatar = $_FILES['userAvatar']['name'];
 }
 
 $sql = "insert into user_tb (userId, username, userSex, userBirthday, userAvatar, password) values ('".$userId."', '".$username."', ".$userSex.", ".$userBirthday.", ".$userAvatar.", '".$password."')";
