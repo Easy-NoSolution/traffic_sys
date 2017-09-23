2b04dfb441a9415e487b662bb80c3e28631e8977
易无解 <2198105373@qq.com>
2017-09-23 12:15:50 +0800

    注册测试

--- register.php
+++ register.php
@@ -10,12 +10,12 @@ require ('connect.php');
 $userId = @$_POST['userId'] ? $_POST['userId'] : NULL;
 $username = @$_POST['username'] ? $_POST['username'] : NULL;
 $userSex = @$_POST['userSex'] ? $_POST['userSex'] : 0;
-$userBirthday = @$_POST['userBirthday'] ? $_POST['userBirthday'] : 'NULL';
+//$userBirthday = @$_POST['userBirthday'] ? $_POST['userBirthday'] : 'NULL';
 //$userBirthday = date('Y-m-d h:i:s');
-//$userBirthday = 'NULL';
-$userAvatar = @$_POST['userAvatar'] ? $_POST['userAvatar'] : 'NULL';
+$userBirthday = 'NULL';
+//$userAvatar = @$_POST['userAvatar'] ? $_POST['userAvatar'] : 'NULL';
 $password = @$_POST['password'] ? $_POST['password'] : NULL;
-//$userAvatar = 'NULL';
+$userAvatar = 'NULL';
 
 if (empty($userId) and empty($username) and empty($userSex) and empty($password)) {
     $json = array('result' => 'Some value is NULL');
