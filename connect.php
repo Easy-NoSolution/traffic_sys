0e2f2344716aa6d6847e4b00097c129f6c9ff0fa
易无解 <2198105373@qq.com>
2017-09-23 12:12:08 +0800

    注册测试

--- connect.php
+++ connect.php
@@ -8,11 +8,13 @@
 
 header("Content-Type:text/html; charset=utf-8");
 header('Access-Control-Allow-Origin:*');
-$connect = mysqli_connect('127.0.0.1', 'root', 'Nsu14310520420', 'traffic_sys');
+$connect = mysqli_connect('127.0.0.1', 'root', 'Nsu14310520420');
 $json = array();
 if (!$connect) {
     $json = array('result' => 'It is failed to connect to database'.mysqli_error($connect));
     exit(json_encode($json));
 }
+
+mysqli_select_db($connect, 'traffic_sys');
 
 ?>
