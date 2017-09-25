e4532496e02833db6b94727abcf820a10757ed84
易无解 <2198105373@qq.com>
2017-09-25 17:32:42 +0800

    注册测试

--- register.php
+++ register.php
@@ -32,9 +32,9 @@ if ($_FILES['userAvatar']['error'] > 0) {
         $json = array('result' => 'It is fail to open userAvatar.png');
         exit(json_encode($json));
     } else {
-        $json = array('result' => 'It is success to open userAvatar', 'fillname' => $fillname, 'userAvatar' => $userAvatar, 'data' => $data);
-        exit(json_encode($json));
         $data = addslashes(fread($fp, filesize($_FILES['userAvatar']['tmp_name'])));
+        $json = array('result' => 'It is success to read userAvatar', 'fillname' => $fillname, 'userAvatar' => $userAvatar, 'data' => $data);
+        exit(json_encode($json));
     }
 }
 
