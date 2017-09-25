527b74dcbd7333af00016b3d279c122027a5470a
易无解 <2198105373@qq.com>
2017-09-25 16:22:38 +0800

    注册测试

--- register.php
+++ register.php
@@ -13,7 +13,7 @@ $userSex = @$_POST['userSex'] ? $_POST['userSex'] : 0;
 //$userBirthday = @$_POST['userBirthday'] ? $_POST['userBirthday'] : 'NULL';
 //$userBirthday = date('Y-m-d h:i:s');
 $userAvatar = @$_FILES['userAvatar']['tmp_name'] ? $_FILES['userAvatar']['tmp_name'] : 'NULL';
-$userAvatar = @addslashes(fread(fopen($userAvatar, "r"), filesize($userAvatar))) ? addslashes(fread(fopen($userAvatar, "r"), filesize($userAvatar))) : 'NULL';
+$data = @addslashes(fread(fopen($userAvatar, "r"), filesize($userAvatar))) ? addslashes(fread(fopen($userAvatar, "r"), filesize($userAvatar))) : 'NULL';
 $userBirthday = 'NULL';
 //$userAvatar = @$_POST['userAvatar'] ? $_POST['userAvatar'] : 'NULL';
 $password = @$_POST['password'] ? $_POST['password'] : NULL;
@@ -24,12 +24,12 @@ if (empty($userId) and empty($username) and empty($userSex) and empty($password)
     exit(json_encode($json));
 }
 
-if ($userAvatar == 'NULL') {
+if ($userAvatar == 'NULL' or $data == 'NULL') {
     $json = array('result' => 'userAvatar value is NULL');
     exit(json_encode($json));
 }
 
-$sql = "insert into user_tb (userId, username, userSex, userBirthday, userAvatar, password) values ('".$userId."', '".$username."', ".$userSex.", ".$userBirthday.", ".$userAvatar.", '".$password."')";
+$sql = "insert into user_tb (userId, username, userSex, userBirthday, userAvatar, password) values ('".$userId."', '".$username."', ".$userSex.", ".$userBirthday.", ".$data.", '".$password."')";
 if (!mysqli_query($connect, $sql)) {
     $json = array('result' => 'It is failed to insert data to database!', 'sql' => $sql);
     exit(json_encode($json));
