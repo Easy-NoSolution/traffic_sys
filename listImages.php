b1b2d72bdeb5dc02cc57280f37713ed2422878b8
易无解 <2198105373@qq.com>
2017-09-26 12:26:14 +0800

    注册测试

--- listImages.php
+++
@@ -1,9 +0,0 @@
-<HTML>
-<HEAD>
-    <TITLE>List BLOB Images</TITLE>
-    <link href="imageStyles.css" rel="stylesheet" type="text/css" />
-</HEAD>
-<BODY>
-<img src="imageView.php?userId=3" /><br/>
-</BODY>
-</HTML>
