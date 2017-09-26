f96131aa9120f704899fc9566734bd821632661a
易无解 <2198105373@qq.com>
2017-09-26 09:54:19 +0800

    注册测试

--- imageUpload.php
+++ imageUpload.php
@@ -18,18 +18,3 @@ if(count($_FILES) > 0) {
             header("Location: listImages.php");
         }}}
 ?>
-<HTML>
-<HEAD>
-    <TITLE>Upload Image to MySQL BLOB</TITLE>
-    <link href="imageStyles.css" rel="stylesheet" type="text/css" />
-</HEAD>
-<BODY>
-<form name="frmImage" enctype="multipart/form-data" action="" method="post"
-      class="frmImageUpload">
-    <label>Upload Image File:</label><br/>
-    <input name="userImage" type="file" class="inputFile" />
-    <input type="submit" value="Submit" class="btnSubmit" />
-</form>
-</div>
-</BODY>
-</HTML>
