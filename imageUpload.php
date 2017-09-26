6e7815e94235f5eb2559506ad6e6762e24a0ef1a
易无解 <2198105373@qq.com>
2017-09-26 10:54:12 +0800

    注册测试

--- imageUpload.php
+++ imageUpload.php
@@ -12,7 +12,7 @@ if(count($_FILES) > 0) {
         $password = 'kfskldjf';
 //        $sql = "INSERT INTO output_images(imageType ,imageData)  VALUES('{$imageProperties['mime']}', '{$imgData}')";
 //        $sql = "insert into user_tb (userId, username, userSex, userBirthday, userAvatar, password) values ('".$userId."', '".$username."', ".$userSex.", ".$userBirthday.", ".$data.", '".$password."')";
-        $sql = "insert into user_tb (userId, username, userSex, userBirthday, userAvatar, password) values ('{$userId}', '{$username}', '{$userSex}', '{$userBirthday}', '{$data}', '$password}')";
+        $sql = "insert into user_tb (userId, username, userSex, userBirthday, userAvatar, password) values ('{$userId}', '{$username}', '{$userSex}', ".$userBirthday.", '{$data}', '$password}')";
 
         $current_id = mysqli_query($connect, $sql) or die("<b>Error:</b> Problem on Image Insert<br/>" .
             mysqli_error($connect));
