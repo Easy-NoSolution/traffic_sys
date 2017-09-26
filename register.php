399aa03cfb96d916d0c91493fda11d8c1bc84a30
易无解 <2198105373@qq.com>
2017-09-26 11:11:25 +0800

    注册测试

--- register.php
+++ register.php
@@ -11,10 +11,8 @@ $userId = @$_POST['userId'] ? $_POST['userId'] : NULL;
 $username = @$_POST['username'] ? $_POST['username'] : NULL;
 $userSex = @$_POST['userSex'] ? $_POST['userSex'] : 0;
 //$userBirthday = @$_POST['userBirthday'] ? $_POST['userBirthday'] : 'NULL';
-//$userBirthday = date('Y-m-d h:i:s');
-
-//$data = @addslashes(fread(fopen($userAvatar, "r"), filesize($userAvatar))) ? addslashes(fread(fopen($userAvatar, "r"), filesize($userAvatar))) : 'NULL';
-$userBirthday = 'NULL';
+$userBirthday = date('Y-m-d h:i:s');
+//$userBirthday = 'NULL';
 //$userAvatar = @$_POST['userAvatar'] ? $_POST['userAvatar'] : 'NULL';
 $password = @$_POST['password'] ? $_POST['password'] : NULL;
 $userAvatar = 'NULL';
@@ -23,22 +21,7 @@ if ($_FILES['userAvatar']['error'] > 0) {
     $json = array('result' => $_FILES['userAvatar']['error']);
     exit(json_encode($json));
 } else {
-    $fillname = $_FILES['userAvatar']['name'];
-    $dotArray = explode('.', $fillname);
-    $type = end($dotArray);
-    $userAvatar = $_FILES['userAvatar']['tmp_name'];
-//    $data = file_get_contents($_FILES['userAvatar']['tmp_name']);
-    $fp = fopen($_FILES['userAvatar']['tmp_name'], 'rb');
-    if (!$fp) {
-        $json = array('result' => 'It is fail to open userAvatar.png');
-        exit(json_encode($json));
-    } else {
-        $size = filesize($_FILES['userAvatar']['tmp_name']);
-        $data = addslashes(fread($fp, filesize($_FILES['userAvatar']['tmp_name'])));
-        fclose($fp);
-        $json = array('result' => 'It is success to read userAvatar', 'fillname' => $fillname, 'userAvatar' => $userAvatar, 'data' => $data, 'size' => $size);
-        exit(json_encode($json));
-    }
+    $userAvatar = addslashes(file_get_contents($_FILES['userAvatar']['tmp_name']));
 }
 
 if (empty($userId) and empty($username) and empty($userSex) and empty($password)) {
@@ -46,7 +29,7 @@ if (empty($userId) and empty($username) and empty($userSex) and empty($password)
     exit(json_encode($json));
 }
 
-$sql = "insert into user_tb (userId, username, userSex, userBirthday, userAvatar, password) values ('".$userId."', '".$username."', ".$userSex.", ".$userBirthday.", ".$data.", '".$password."')";
+$sql = "insert into user_tb (userId, username, userSex, userBirthday, userAvatar, password) values ('{$userId}', '{$username}', '{$userSex}', '{$userBirthday}', '{$userAvatar}', '{$password}')";
 if (!mysqli_query($connect, $sql)) {
     $json = array('result' => 'It is failed to insert data to database!', 'sql' => $sql);
     exit(json_encode($json));
