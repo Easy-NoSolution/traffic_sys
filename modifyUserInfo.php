6bca916116dbccb3820ac46e0e19bad64c264912
易无解 <2198105373@qq.com>
2017-10-09 15:29:55 +0800

    修改返回结果bug

---
+++ modifyUserInfo.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yiwujie
+ * Date: 09/10/2017
+ * Time: 14:05
+ */
+
+require ("connect.php");
+$userId = @$_POST['userId'] ? $_POST['userId'] : null;
+$username = @$_POST["username"] ? $_POST["username"] : null;
+$userSex = @$_POST['userSex'] ? $_POST['userSex'] : null;
+//$userBirthday = @$_POST['userBirthday'] ? $_POST['userBirthday'] : null;
+$userBirthday = date('Y-m-d h:i:s');
+$userAvatar = null;
+$json = array();
+
+if ($_FILES['userAvatar']['error'] > 0) {
+    $json = array('result' => 'failed', 'errorInfo' => $_FILES['userAvatar']['error']);
+    exit(json_encode($json));
+} else {
+    $userAvatar = addslashes(file_get_contents($_FILES['userAvatar']['tmp_name']));
+}
+if (empty($username) and empty($userSex)) {
+    $json = array('result' => 'failed', 'errorInfo' => 'Some value is null');
+    exit(json_encode($json));
+}
+
+$sql = "update user_tb set username = '{$username}' where userId = '{$userId}'";
+$result = mysqli_query($connect, $sql);
+if (!$result) {
+    $json = array('result' => 'failed', 'errorInfo' => 'It is failed to modify username');
+    exit(json_encode($json));
+}
+$sql = "update user_tb set userSex = '{$userSex}' where userId = '{$userSex}'";
+$result = mysqli_query($connect, $sql);
+if (!$result) {
+    $json = array('result' => 'failed', 'errorInfo' => 'It is failed to modify userSex');
+    exit(json_encode($json));
+}
+//$sql = "update user_tb set userBirthday = '{$userBirthday}' where userId = '{$userBirthday}'";
+//$result = mysqli_query($connect, $sql);
+//if (!$result) {
+//    $json = array('result' => 'failed', 'errorInfo' => 'It is failed to modify userBirthday');
+//    exit(json_encode($json));
+//}
+$sql = "update user_tb set userAvatar = '{$userAvatar}' where userId = '{$userAvatar}'";
+$result = mysqli_query($connect, $sql);
+if (!$result) {
+    $json = array('result' => 'failed', 'errorInfo' => 'It is failed to modify userAvatar');
+    exit(json_encode($json));
+}
+
+$json = array('result' => 'success');
+exit(json_encode($json));
+?>
+
