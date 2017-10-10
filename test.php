a1654f5dade335835f363550052284e7c358dad6
易无解 <2198105373@qq.com>
2017-10-10 16:15:54 +0800

    将日期存为时间戳

--- test.php
+++ test.php
@@ -17,8 +17,10 @@ if ($_FILES['userAvatar']['error'] > 0) {
     $fillname = $_FILES['userAvatar']['name'];
     $dotArray = explode('.', $fillname);
     $type = end($dotArray);
-    $path = "../usr/local/apache/htdocs/traffic_sys_pictures/" . $userId . '.' . $type;
+//    $path = "../usr/local/apache/htdocs/traffic_sys_pictures/" . $userId . '.' . $type;
+    $path = "../" . $userId . '.' . $type;
     move_uploaded_file($_FILES['userAvatar']['tmp_name'], $path);
+
     $json = array('result' => 'success', 'fillname' => $fillname, 'userId' => $userId, 'type' => $type, 'path' => $path);
     exit(json_encode($json));
 }
