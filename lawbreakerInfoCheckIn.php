8cb5aaaae06983e49614bb2581a819bfff225963
易无解 <2198105373@qq.com>
2017-10-20 09:39:30 +0800

    完成所有的后台接口设计

--- lawbreakerInfoCheckIn.php
+++ lawbreakerInfoCheckIn.php
@@ -13,12 +13,12 @@ $carOwnerId = @$_POST['carOwnerId'] ? $_POST['carOwnerId'] : null;
 $lawbreakerInfo = @$_POST['lawbreakerInfo'] ? $_POST['lawbreakerInfo'] : null;
 
 $json = array();
-if (empty($carId) and empty($carOwnerId) and empty($lawbreakerInfo)) {
+if (empty($carId) or empty($carOwnerId) or empty($lawbreakerInfo)) {
     $json = array('result' => 'failed', 'errorInfo' => 'Some value is null');
     exit(json_encode($json));
 }
 
-$sql = "insert into car_tb (carId, carOwnerId, lawbreakerInfo) VALUES ('{$carId}', '{$carOwnerId}', '{$lawbreakerInfo}')";
+$sql = "insert into lawbreakerInfo_tb (carId, carOwnerId, lawbreakerInfo) VALUES ('{$carId}', '{$carOwnerId}', '{$lawbreakerInfo}')";
 if (!mysqli_query($connect, $sql)) {
     $json = array('result' => 'failed', 'errorInfo' => 'It is failed to insert data to database!', 'sql' => $sql);
     exit(json_encode($json));
